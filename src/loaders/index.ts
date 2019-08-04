f0a9e84e85d4cfa6df9ba7a0f31f13aba93a9def
neobooru <50623835+neobooru@users.noreply.github.com>
2019-08-05 01:42:33 +0200

    Add konachan and yande.re (Moebooru)

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,3 +1,4 @@
 export { default as Gelbooru } from "./Gelbooru";
 export { default as Danbooru } from "./Danbooru";
 export { default as Shimmie2 } from "./Shimmie2";
+export { default as Moebooru } from "./Moebooru";
