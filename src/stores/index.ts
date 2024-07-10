45f20ebfc8061c2487e78ea63930c92a93d60be1
G1org1owo <77230371+G1org1owo@users.noreply.github.com>
2024-07-11 00:17:17 +0200

    Add config interface for tag ignores

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,6 +1,7 @@
 import { defineStore } from "pinia";
 import deepMerge from "deepmerge";
 import {
+  type TagDetails,
   getDefaultTagCategories,
   type ScrapedPostDetails,
   type SetPostUploadInfoData,
@@ -40,6 +41,7 @@ export const cfg = useStorageLocal(
       showInstancePicker: true,
     },
     tagCategories: [] as Array<TagCategoryColor>,
+    tagIgnores: [] as Array<TagDetails>,
   },
   {
     mergeDefaults(storageValue, defaults) {
